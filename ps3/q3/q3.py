ca83654822391093256bae0da9bbb1dd7d1e5992
Mohan Agrawal <mohan.agr97@gmail.com>
2021-10-08 17:31:02 -0400

    adds q3 and readme draft 1

--- ps3/q3/q3.py
+++ ps3/q3/q3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 class leastsq():
     
@@ -55,6 +56,7 @@ if __name__ == '__main__':
 
     data = np.loadtxt('./dish_zenith.txt')
 
+    print("*** Identity Noise matrix ***\n")
     A = np.zeros((data.shape[0],4))
     A[:,0] = data[:,0]**2 + data[:,1]**2 # x^2 + y^2
     A[:,1] = data[:,0]
@@ -65,14 +67,80 @@ if __name__ == '__main__':
     lsq = leastsq(A,y)
     lsq.fit()
     m = lsq.params
+
     ypred = A@m
     resd = y-ypred
-    rmserr = np.sqrt(np.mean(resd**2))
+
+    rmserr = np.sqrt(np.sum(resd**2)/(y.shape[0]-m.shape[0]+1))
+    chisq = resd.T@resd # Noise is identity right now
     a = m[0]
     x0 = -0.5*m[1]/a
     y0 = -0.5*m[2]/a
     z0 = m[3]-x0**2-y0**2
     print(f"params of the paraboloid are:\na\t= {a:4.2e}\nx0\t= {x0:4.2f}\ny0\t= {y0:4.2f}\nz0\t= {z0:4.2f}")
-    print(f"\nRMS error or in this case chisq of fit is: {rmserr:4.2f}")
+    print(f"Chisq of fit is: {chisq:4.2f}")
+    print(f"RMS studentized err is: {rmserr:4.2f} mm")
+
+    # let's estimate a better noise model
+
+    print("\n*** Repeating with a better Noise model (refer readme) ***\n")
+
+    bin_std, bin_edges, bin_num = stats.binned_statistic(y,resd,statistic='std')
+
+    #some nice plotting
+    fig, ax = plt.subplots(1,1)
+    fig.set_size_inches(10,8)
+    ax.set_title('Residuals at each "y"')
+    ax.plot(y, resd,'b.', label="error")
+    ax.axhline(resd.mean(), c='r', label='Global mean')
+    for edge in bin_edges:
+        c = ax.axvline(edge, c='g',linestyle='--')
+    handles, labels = ax.get_legend_handles_labels()
+    handles.append(c)
+    labels.append('bin edge')
+    leg = ax.legend(handles, labels)
+    ax.set_xlabel('y[i]')
+    ax.set_ylabel('err at y[i]')
+    fig.savefig('./residual_variation.png')
+
+
+    # let's create a new noise matrix with each diagonal element =  square of avg error of each bin
+
+    Nnew = np.eye(y.shape[0])
+    for i in range(0, len(bin_edges)-1):
+        idx = np.where(np.logical_and(y>bin_edges[i], y<=bin_edges[i+1]))
+        Nnew[idx,idx] = bin_std[i]**2
+    # set the first element separately
+    idx = np.where(y==bin_edges[0])
+    Nnew[idx,idx] = bin_std[0]**2
+
+    lsq = leastsq(A,y,Nnew)
+    lsq.fit()
+    m = lsq.params
+    a = m[0]
+    x0 = -0.5*m[1]/a
+    y0 = -0.5*m[2]/a
+    z0 = m[3]-x0**2-y0**2
+
+    ypred = A@m
+    resd = y-ypred
+    rmserr = np.sqrt(np.mean(resd**2))
+    chisq = resd.T@np.linalg.inv(Nnew)@resd
+    print(f"params of the paraboloid are:\na\t= {a:4.2e}\nx0\t= {x0:4.2f}\ny0\t= {y0:4.2f}\nz0\t= {z0:4.2f}")
+    print(f"Chisq of fit is: {chisq:4.2f}")
+    print(f"RMS studentized err is: {rmserr:4.2f} mm")
+
+    mcov = lsq.get_param_cov()
+    merr = np.sqrt(np.diag(mcov))
+
+    err_a = merr[0]
+
+    err_focus = np.abs(0.25*err_a/a**2)
+
+    print(f"Error in focus is {err_focus:4.2f} mm")
+
+
+
+
 
 
---
+++ ps3/q3/readme.md
@@ -0,0 +1,35 @@
+## Noise model
+
+If there are two models with differening chisquare values, we prefer the one with lesser chisquare -- as it's more probable that the data we see came from the model with lesser chisquare.
+
+`P(d|m1) > P(d|m2)` if `chisq(m1) < chisq(m2)`
+
+When starting with the default model of Identity Noise matrix, (which translates to assuming that the error in our estimates are distributed as standard normal `N(0,1)`), we obtain a chisquare of 6745. In search for a new noise model, we first plot the residuals with the values of the dependent variable (called `y` in the code and `d` in Jon's notes). This is to visualize if the variation of error in our predictions -- small for some points, larger for others.
+
+It is apparent from `residual_variation.png` that there is a _heteroskedastic_ trend in our residuals, i.e. the variation of residuals is: 
+    1. Not constant
+    2. Not unity
+
+The simplest update to our noise matrix would be to replace `N(i,i) = [residual(i)]**2`. However, this scheme is susceptible to failure if residual for some of our points is ~ 0. These quantities would blow up in `inv(N)`, and would bias our model too much. We could get a slightly better solution if our points could "communicate" with their neighbours. This would ensure that no one point has disproportionate effect on the model.
+
+A way to achieve such a model is to bin the data and assign the average error in each bin as the error for those points in the nosie matrix. There are two ways to estimate the said "error". We could either:
+    - Calculate the variance of residuals in the bin.
+    - Take the mean of the square of the residuals.
+
+In practice, both of the above ways yield similar answers because `Var(e) = <e^2> - <e>^2`, and the second term on RHS (the mean of residuals) in zero or close to zero in most cases (including ours).
+
+## Result
+
+With the new noise matrix, our chisquare reduces to 441 -- improvement of a factor of 15! This model can now be used to estimate model parameters and uncertainties.
+
+## Bonus
+
+There are two parts to the bonus question.
+
+    1. Introducing non-circularity in the paraboloid.
+        - In this case, the coefficients of `(x-x0)^2` and `(y-y0)^2` will be different, unlike in the previous case. If we look down on the paraboloid (facing the bottom), we'll see an ellipse rather than a circle.
+
+    2. Introducing asymmetric axes.
+        - In this case, the paraboloid's principal axes are rotated with respect to ground. A simple coordinate transformation using 2-D rotation matrix will yield the correct coordinates.
+
+First of the above two cases has been implemented in the code. Second part, though straightforward, has been skipped because of time-constraints.
