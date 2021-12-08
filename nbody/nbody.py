0ab51baeb91d7a05f0edb004d77dbf58e4eb69f8
Mohan <mohan.agr97@gmail.com>
2021-12-08 15:39:31 -0500

    bad commit

--- nbody/nbody.py
+++ nbody/nbody.py
@@ -8,23 +8,30 @@ import sys
 
 np.random.seed(42)
 
-@nb.njit(parallel=True)
+# @nb.njit(parallel=True)
 def get_grad(x,pot,RES):
     PE = 0
     nside = pot.shape[0]
     npart = x.shape[0]
     grad = np.zeros((npart,2))
+
     for i in nb.prange(npart):
         # print("Particle positions:", x[i])
-        irow = int(nside // 2 - x[i, 1] // RES - 1)  # row num is given by y position up down
+        if(np.abs(x[i,0])>RES*nside//2): x[i,0] = np.round(x[i,0]) # save yourself some roundoff errors
+        if (np.abs(x[i, 1]) > RES * nside // 2): x[i, 1] = np.round(x[i, 1])
+
+        irow = int((nside -1)/ 2 - x[i, 1] * (nside-1)/(nside*RES) )  # row num is given by y position up down
         # but y made to vary 16 to -16 down. [0] is 16
-        icol = int(nside // 2 + x[i, 0] // RES)  # col num is given by x position left right
+        icol = int((nside -1)/ 2 + x[i, 0] * (nside-1)/(nside*RES) )  # col num is given by x position left right
         # print("In indices ffrom grad", irow, icol, "actually", rho[irow,icol])
         # print(np.where(rho>0))
         #         print("pot 1 term", pot[(ix+1)%nside,iy])
         #         print("pot 2 term", pot[ix-1,iy])
+        # print(x[i, 0], x[i, 1])
+        # print(irow, icol)
         grad[i, 1] = 0.5 * (pot[(irow - 1), icol] - pot[(irow + 1) % nside, icol]) / RES
         # y decreases with high row num.
+
         grad[i, 0] = 0.5 * (pot[irow, (icol + 1) % nside] - pot[irow, icol - 1]) / RES
         # print(self.grad)
     return -grad
@@ -63,6 +70,7 @@ class nbody():
         self.XMIN = xmin
         self.RES = (self.XMAX-self.XMIN)/self.nside
         self.fac = 1
+        self.mask = np.ones(npart,dtype='bool')
 
         # set up the self.kernel here if soft length remains constant
         self.set_kernel()
@@ -119,6 +127,9 @@ class nbody():
     def enforce_period(self, x):
         x[np.logical_or(x > self.XMAX, x < self.XMIN)] = x[np.logical_or(x > self.XMAX, x < self.XMIN)] % (self.XMAX - self.XMIN)
         x[x > self.XMAX] = x[x > self.XMAX] - (self.XMAX - self.XMIN)
+        # print("from enforce", x.shape, x[:,0].min(), x[:,0].max())
+
+        return x
 
     def set_grad(self):
         for i in range(self.npart):
@@ -136,15 +147,16 @@ class nbody():
 
 
 
-    def update_rho(self, x):
+    def update_rho(self):
         bins = self.RES*(np.arange(self.nside+1)-self.nside//2) #can equivalentlly use fftshift
         # since each cell is a bin, we have Nside bins, need Nside+1 points
 
-        self.enforce_period(x)
+        self.x[self.mask] = self.enforce_period(self.x[self.mask]) # had to figure out the hard way : with fancy indexing numpy doesnt always return a view, it's a copy
+        # print("after enforce", self.x.shape, self.x[:, 0].min(), self.x[:, 0].max())
         # self.rho, xedges, yedges = np.histogram2d(x[:, 1], x[:, 0], bins=bins)
         # self.rho = np.flipud(self.rho).copy()
         self.rho[:] = 0
-        hist2d(x,self.rho,self.RES)
+        hist2d(self.x[self.mask],self.rho,self.RES)
         # print(bins, self.RES)
         # we want y as top down, x as left right, and y starting 16 at top -16 at bottom
         self.rhoft = np.fft.rfft2(self.rho)
@@ -153,18 +165,18 @@ class nbody():
         self.pot = np.fft.irfft2(self.rhoft * self.kernelft, [self.nside, self.nside])
 
     def update_forces(self):
-        self.f[:] = get_grad(self.x, self.pot, self.RES)
+        self.f[self.mask] = get_grad(self.x[self.mask], self.pot, self.RES)
         # return get_grad(self.x, self.pot, self.RES)
 
     def run_leapfrog(self, dt=1, verbose=False):
-        KE = 0.5 * np.sum(self.v ** 2)
+        KE = 0.5 * np.sum(self.v[self.mask] ** 2)
         PE1 = 0.5 * np.sum(obj.rho * obj.pot)
-        self.x[:] = self.x[:] + dt * self.v
-        self.update_rho(self.x)
+        self.x[self.mask] = self.x[self.mask] + dt * self.v[self.mask]
+        self.update_rho()
         self.update_pot()
         PE2 = 0.5*np.sum(obj.rho * obj.pot)
         self.update_forces()
-        self.v[:]=self.v[:]+self.f*dt
+        self.v[self.mask]=self.v[self.mask]+self.f[self.mask]*dt # set it up such that dimension of f changes
 
         PE = 0.5*(PE1+PE2)
         if(verbose):
@@ -174,7 +186,7 @@ class nbody():
     def run_leapfrog2(self, dt=1, verbose=False):
 
         PE = 0.5 * np.sum(self.rho * self.pot)
-        KE = 0.5*np.sum(self.v[:]**2)
+        KE = 0.5*np.sum(self.v[self.mask]**2)
         # vhalf = np.zeros((self.npart,2))
         #
         # cur_f = get_grad(self.x, self.pot, self.RES).copy()
@@ -190,12 +202,12 @@ class nbody():
         # cur_f[:] = new_f[:]
 
 
-        self.x[:] = self.x[:] + dt * self.v[:] + self.cur_f * dt * dt /2
-        self.update_rho(self.x)
+        self.x[self.mask] = self.x[self.mask] + dt * self.v[self.mask] + self.cur_f * dt * dt /2
+        self.update_rho(self.x[self.mask])
         self.update_pot()
-        self.new_f[:] = get_grad(self.x, self.pot, self.RES)
-        self.v[:] = self.v[:] + (self.cur_f + self.new_f)*dt/2
-        self.cur_f[:] = self.new_f[:]
+        self.new_f = get_grad(self.x[self.mask], self.pot, self.RES)
+        self.v[self.mask] = self.v[self.mask] + (self.cur_f + self.new_f)*dt/2
+        self.cur_f = self.new_f
         return PE+KE
 
 
@@ -244,21 +256,21 @@ if(__name__=="__main__"):
     XMAX=128
     XMIN=-128
     RES=(XMAX-XMIN)/NSIDE
-    NPART = 1
+    NPART = 50000
     SOFT = 1
     TSTEP = SOFT * RES / np.sqrt(NPART)  # this is generally smaller than eps**3/2
     obj = nbody(NPART,XMAX,XMIN,soft=SOFT,nside=NSIDE)
 
 
-    obj.ic_1part()
+    # obj.ic_1part()
     # obj.ic_2part_circular()
     # obj.run(dt=0)
     # print(obj.grad)
-    # v = np.sqrt(NPART*0.6/XMAX)
-    # obj.ics_2gauss(v) #to use this change lims to +/- 128 and particles to 5000
+    v = np.sqrt(NPART*0.6/XMAX)
+    obj.ics_2gauss(v) #to use this change lims to +/- 128 and particles to 5000
     # obj.ic_3part()
     # obj.ic_gauss()
-    obj.update_rho(obj.x)
+    obj.update_rho()
     obj.update_pot()
     obj.cur_f[:] = get_grad(obj.x,obj.pot,obj.RES)
     frames = []  # for storing the generated images
@@ -269,9 +281,9 @@ if(__name__=="__main__"):
     try:
         for i in range(20000):
 
-            TE = obj.run_leapfrog2(dt=TSTEP)
-            print(obj.x)
-            print(TE)
+            TE = obj.run_leapfrog(dt=TSTEP)
+            # print(obj.x)
+
             if(i%10==0):
                 # print("iter", i)
                 # if (TE_old != 0):
@@ -282,6 +294,7 @@ if(__name__=="__main__"):
                 #         fac = fac * 2
                     # print(np.abs((TE - TE_old) / TE_old))
                 #update TE_old=TE at the end
+                print(TE)
                 logfile.write(str(TE)+"\n")
                 # plt.clf()
                 frames.append([plt.imshow(obj.rho**0.5,cmap='inferno',animated=True)])
@@ -292,7 +305,7 @@ if(__name__=="__main__"):
                 labels = -positions*RES+XMAX  # labels you want to see --- imshow plots origin at top
                 plt.yticks(positions, labels)
                 plt.xticks(positions, labels)
-                plt.pause(0.5)
+                plt.pause(0.0001)
     except KeyboardInterrupt as e:
         pass
     finally:
